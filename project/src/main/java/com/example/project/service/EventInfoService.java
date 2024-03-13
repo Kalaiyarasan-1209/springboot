6fffadf6d126248e5db65df3d40abef2fd28552b
Kalaiyarasan-1209 <160819111+Kalaiyarasan-1209@users.noreply.github.com>
2024-03-13 09:28:53 +0530

    Add files via upload

---
+++ project/src/main/java/com/example/project/service/EventInfoService.java
@@ -0,0 +1,26 @@
+package com.example.project.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.example.project.model.EventInfo;
+import com.example.project.repository.EventInfoRepo;
+import java.util.List;
+
+@Service
+public class EventInfoService {
+    public EventInfoRepo eventInfoRepo;
+    public EventInfoService(EventInfoRepo eventInfoRepo) {
+        this.eventInfoRepo = eventInfoRepo;
+    }
+    public EventInfo posting(EventInfo eventInfo)
+    {
+        return eventInfoRepo.save(eventInfo);
+    }
+    public List<EventInfo> get()
+    {
+        return eventInfoRepo.findAll();
+    }
+
+}
